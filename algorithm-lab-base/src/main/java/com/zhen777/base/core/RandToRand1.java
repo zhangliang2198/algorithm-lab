4189a83ec5bdebf12f8d813ceba0568766df131e
张亮 <zhangl02@szlanyou.com>
2023-02-24 00:04:50 +0800

    -- sort and binary

---
+++ algorithm-lab-base/src/main/java/com/zhen777/base/core/RandToRand1.java
@@ -0,0 +1,30 @@
+package com.zhen777.base.core;
+
+import com.zhen777.base.Util;
+
+import java.util.Random;
+
+public class RandToRand1 {
+    public static void main(String[] args) {
+        int testTimes = 100000;
+        int arr[] = new int[2];
+        for (int i = 0; i < testTimes; i++) {
+            arr[f2()]++;
+        }
+        Util.printArray(arr);
+    }
+
+    // 概率不等的发生器
+    private static int f1() {
+        return Math.random() < 0.83 ? 1 : 0;
+    }
+
+    // 概率相等等的发生器
+    private static int f2() {
+        int ans;
+        do {
+            ans = f1();
+        } while (ans == f1());
+        return ans;
+    }
+}
