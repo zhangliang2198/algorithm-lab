1ec0301325992b3cd1c94d9ecf44f3d577c18b6f
张亮 <zhangl02@szlanyou.com>
2023-02-23 23:48:12 +0800

    -- sort and binary

---
+++ algorithm-lab-base/src/main/java/com/zhen777/base/core/RandToRand.java
@@ -0,0 +1,49 @@
+package com.zhen777.base.core;
+
+import com.zhen777.base.Util;
+
+public class RandToRand {
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int res[] = new int[10];
+        for (int i = 0; i < testTime; i++) {
+            res[getNewIntRandom1()]++;
+        }
+        Util.printArray(res);
+    }
+
+    // 得到 0-7 等概率
+    private static int getNewIntRandom() {
+        int n = 5;
+        int res = 0;
+        return (getBinary(n) << 2) + (getBinary(n) << 1) + (getBinary(n));
+    }
+
+    // 得到 0-8 等概率
+    private static int getNewIntRandom1() {
+        int n = 5;
+        int res = (getBinary(n) << 3) + (getBinary(n) << 2) + (getBinary(n) << 1) + (getBinary(n));
+        if (res <= 8) {
+            return res;
+        }
+        return getNewIntRandom1();
+    }
+
+    // 由getRandom 1-5 的随机，产生一个0，1发生器
+    private static int getBinary(int n) {
+        int random = getRandom(n);
+        int mid = n / 2;
+        if (random <= mid) {
+            return 0;
+        } else if (random > mid + (n % 2)) {
+            return 1;
+        }
+        return getBinary(n);
+    }
+
+    // 等概率得到1 - n之间的随机数
+    private static int getRandom(int n) {
+        // 算落到 [0,x) 不方便计算，算不落到落到 [0,x)的概率
+        return 1 + (int) (Math.random() * n);
+    }
+}
---
+++ algorithm-lab-base/src/main/java/com/zhen777/base/core/Random.java
@@ -0,0 +1,65 @@
+package com.zhen777.base.core;
+
+import com.zhen777.base.Util;
+
+/**
+ * 随机
+ *
+ * @author zhang
+ * @date 2023/02/23
+ */
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("测试开始");
+        // Math.random() --> [0,1) 等概率
+
+        // 测试等概率
+        int testTimes = 10000000;
+        int count = 0;
+        for (int i = 0; i < testTimes; i++) {
+            if (Math.random() < 0.3) {
+                count++;
+            }
+        }
+        System.out.println((double) count / (double) testTimes);
+
+        // 一种算区间的概率方式 8等分，大于5的概率
+        System.out.println("===========================");
+        count = 0;
+        for (int i = 0; i < testTimes; i++) {
+            if (Math.random() * 8 > 5) {
+                count++;
+            }
+        }
+        System.out.println((double) (8 - 5) / (double) 8);
+        System.out.println((double) count / (double) testTimes);
+
+        // Math.random() * N  =  [0,N)等概率 ，再转换成整形 [0,N-1]
+        // 验证
+        System.out.println("===========================");
+        int[] ans = new int[9];
+        for (int i = 0; i < testTimes; i++) {
+            int in = (int) (Math.random() * 9);
+            ans[in]++;
+        }
+        Util.printArray(ans);
+
+        System.out.println("===========================");
+        // 对于 x ，[0,x) 出现的概率是 x平方，怎么计算
+        count = 0;
+        double test = 0.7;
+        for (int i = 0; i < testTimes; i++) {
+            // 2 次都落在  0.7 范围内，相当于平方了，同理 3次方一样的道理
+            if (xToXPower2() < test) {
+                count++;
+            }
+        }
+        System.out.println((double) count / (double) testTimes);
+        System.out.println(test * test);
+    }
+
+    private static double xToXPower2() {
+        // 算落到 [0,x) 不方便计算，算不落到落到 [0,x)的概率
+        return Math.max(Math.random(), Math.random());
+    }
+}
