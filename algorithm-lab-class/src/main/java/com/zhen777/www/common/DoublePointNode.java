011077158905a41786573251f088697ff6275462
mikoozhang <48906088@qq.com>
2023-03-02 19:38:36 +0800

    --单链表翻转

---
+++ algorithm-lab-class/src/main/java/com/zhen777/www/common/DoublePointNode.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.common;
+
+/**
+ * 双点节点.
+ *
+ * @author mikoozhang[zhangliang2198@outlook.com]
+ * @version 1.0.0
+ */
+public class DoublePointNode {
+    public DoublePointNode prev;
+    public DoublePointNode next;
+    public Integer data;
+}
---
+++ algorithm-lab-class/src/main/java/com/zhen777/www/common/SinglePointNode.java
@@ -0,0 +1,10 @@
+package com.zhen777.www.common;
+
+public class SinglePointNode {
+    public SinglePointNode next;
+    public Integer data;
+
+    public SinglePointNode(Integer data) {
+        this.data = data;
+    }
+}
